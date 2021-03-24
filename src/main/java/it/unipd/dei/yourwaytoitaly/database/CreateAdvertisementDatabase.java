292324cbec21fc45b957a5bec499af5a1cc267a3
Vittorio Esposito <vittorio.esposito.1@studenti.unipd.it>
2021-03-24 18:41:47 +0100

    .

---
+++ src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateAdvertisementDatabase.java
@@ -0,0 +1,8 @@
+package it.unipd.dei.yourwaytoitaly.database;
+
+import it.unipd.dei.yourwaytoitaly.resource.DatabaseEntities;
+import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
