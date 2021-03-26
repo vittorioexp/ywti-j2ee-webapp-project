3cc497648bb026ceaad917ac979cc0a2a7cade54
pivamatteo <74982764+pivamatteo@users.noreply.github.com>
2021-03-26 18:27:09 +0100

    New CREATE statements with some changes

--- src/database/CreateTables.sql
+++ src/database/CreateTables.sql
@@ -1,118 +1,86 @@
 CREATE TABLE City (
- ID_city INT DEFAULT AUTO_INCREMENT NOT NULL,
+ ID_city VARCHAR(20) PRIMARY KEY,
  name VARCHAR(100) NOT NULL
 );
-
-ALTER TABLE City ADD CONSTRAINT PK_City PRIMARY KEY (ID_city);
 
 
 CREATE TABLE Type_advertisement (
- ID_type INT DEFAULT AUTO_INCREMENT NOT NULL,
- name VARCHAR(100) NOT NULL
+ ID_type VARCHAR(50) PRIMARY KEY,
+ description VARCHAR(100) 
 );
-
-ALTER TABLE Type_advertisement ADD CONSTRAINT PK_Type_advertisement PRIMARY KEY (ID_type);
-
-
-CREATE TABLE User (
- ID_user INT DEFAULT AUTO_INCREMENT NOT NULL,
- email VARCHAR(100) NOT NULL,
- phone_number VARCHAR(100) NOT NULL,
- address VARCHAR(100) NOT NULL,
- password VARCHAR(100) NOT NULL,
- user_name VARCHAR(100) NOT NULL,
- ID_city INT DEFAULT AUTO_INCREMENT NOT NULL
-);
-
-ALTER TABLE User ADD CONSTRAINT PK_User PRIMARY KEY (ID_user);
 
 
 CREATE TABLE Company (
- ID_user INT DEFAULT AUTO_INCREMENT NOT NULL,
- denomination VARCHAR(100) NOT NULL
+ email_c VARCHAR(100) PRIMARY KEY,
+ name_c VARCHAR(100) NOT NULL,
+ phone_number VARCHAR(100) NOT NULL,
+ address VARCHAR(100) NOT NULL,
+ password VARCHAR(100) NOT NULL,
+ ID_city VARCHAR(20) NOT NULL,
+ FOREIGN KEY (ID_city) REFERENCES City (ID_city)
 );
-
-ALTER TABLE Company ADD CONSTRAINT PK_Company PRIMARY KEY (ID_user);
 
 
 CREATE TABLE Tourist (
- ID_user INT DEFAULT AUTO_INCREMENT NOT NULL,
+ email_t VARCHAR(100)PRIMARY KEY,
  surname VARCHAR(100) NOT NULL,
  name VARCHAR(100) NOT NULL,
- birth_date DATE NOT NULL
+ birth_date DATE NOT NULL,
+ phone_number VARCHAR(100) NOT NULL,
+ address VARCHAR(100) NOT NULL,
+ password VARCHAR(100) NOT NULL,
+ ID_city VARCHAR(20) NOT NULL,
+ FOREIGN KEY (ID_city) REFERENCES City (ID_city)
 );
-
-ALTER TABLE Tourist ADD CONSTRAINT PK_Tourist PRIMARY KEY (ID_user);
 
 
 CREATE TABLE Advertisement (
- ID_advertisement INT DEFAULT AUTO_INCREMENT NOT NULL,
- ID_user INT DEFAULT AUTO_INCREMENT NOT NULL,
- ID_type INT DEFAULT AUTO_INCREMENT NOT NULL,
+ ID_advertisement INT PRIMARY KEY,
  description VARCHAR(10000) NOT NULL,
  score INT NOT NULL,
  price INT NOT NULL,
  num_tot_item INT NOT NULL,
  date_start DATE NOT NULL,
  date_end DATE,
- time_start TIME(10),
- time_end TIME(10)
+ time_start TIME,
+ time_end TIME,
+ email_c VARCHAR(100) NOT NULL,
+ ID_type VARCHAR(50) NOT NULL,
+ FOREIGN KEY (email_c) REFERENCES Company (email_c),
+ FOREIGN KEY (ID_type) REFERENCES Type_advertisement (ID_type)
 );
-
-ALTER TABLE Advertisement ADD CONSTRAINT PK_Advertisement PRIMARY KEY (ID_advertisement);
 
 
 CREATE TABLE Booking (
- ID_user INT DEFAULT AUTO_INCREMENT NOT NULL,
- ID_advertisement INT DEFAULT AUTO_INCREMENT NOT NULL,
- date DATE NOT NULL,
- time TIME(10) NOT NULL,
- num_booking INT NOT NULL
+ PRIMARY KEY (email_t,ID_advertisement,date_b),
+ date_b DATE NOT NULL,
+ time_b TIME NOT NULL,
+ num_booking INT NOT NULL,
+ state VARCHAR(20) NOT NULL,
+ email_t VARCHAR(100) NOT NULL,
+ ID_advertisement INT  NOT NULL,
+ FOREIGN KEY (email_t) REFERENCES Tourist (email_t),
+ FOREIGN KEY (ID_advertisement) REFERENCES Advertisement (ID_advertisement)
 );
-
-ALTER TABLE Booking ADD CONSTRAINT PK_Booking PRIMARY KEY (ID_user,ID_advertisement);
 
 
 CREATE TABLE FeedBack (
- ID_user INT DEFAULT AUTO_INCREMENT NOT NULL,
- ID_advertisement INT DEFAULT AUTO_INCREMENT NOT NULL,
- rate INT,
- text VARCHAR(10000)
+ PRIMARY KEY (email_t,ID_advertisement,date_f),
+ rate INT NOT NULL,
+ text_f VARCHAR(10000),
+ date_f DATE NOT NULL,
+ email_t VARCHAR(100) NOT NULL,
+ ID_advertisement INT  NOT NULL,
+ FOREIGN KEY (email_t) REFERENCES Tourist (email_t),
+ FOREIGN KEY (ID_advertisement) REFERENCES Advertisement (ID_advertisement)
 );
-
-ALTER TABLE FeedBack ADD CONSTRAINT PK_FeedBack PRIMARY KEY (ID_user,ID_advertisement);
 
 
 CREATE TABLE Image (
- ID_image INT DEFAULT AUTO_INCREMENT NOT NULL,
- path VARCHAR(10000) NOT NULL,
- ID_advertisement INT DEFAULT AUTO_INCREMENT NOT NULL
+ ID_image INT PRIMARY KEY,
+ path_i VARCHAR(10000) NOT NULL,
+ description_i VARCHAR(100),
+ ID_advertisement INT  NOT NULL,
+ FOREIGN KEY (ID_advertisement) REFERENCES Advertisement (ID_advertisement)
 );
-
-ALTER TABLE Image ADD CONSTRAINT PK_Image PRIMARY KEY (ID_image);
-
-
-ALTER TABLE User ADD CONSTRAINT FK_User_0 FOREIGN KEY (ID_city) REFERENCES City (ID_city);
-
-
-ALTER TABLE Company ADD CONSTRAINT FK_Company_0 FOREIGN KEY (ID_user) REFERENCES User (ID_user);
-
-
-ALTER TABLE Tourist ADD CONSTRAINT FK_Tourist_0 FOREIGN KEY (ID_user) REFERENCES User (ID_user);
-
-
-ALTER TABLE Advertisement ADD CONSTRAINT FK_Advertisement_0 FOREIGN KEY (ID_user) REFERENCES Company (ID_user);
-ALTER TABLE Advertisement ADD CONSTRAINT FK_Advertisement_1 FOREIGN KEY (ID_type) REFERENCES Type_advertisement (ID_type);
-
-
-ALTER TABLE Booking ADD CONSTRAINT FK_Booking_0 FOREIGN KEY (ID_user) REFERENCES Tourist (ID_user);
-ALTER TABLE Booking ADD CONSTRAINT FK_Booking_1 FOREIGN KEY (ID_advertisement) REFERENCES Advertisement (ID_advertisement);
-
-
-ALTER TABLE FeedBack ADD CONSTRAINT FK_FeedBack_0 FOREIGN KEY (ID_user) REFERENCES Tourist (ID_user);
-ALTER TABLE FeedBack ADD CONSTRAINT FK_FeedBack_1 FOREIGN KEY (ID_advertisement) REFERENCES Advertisement (ID_advertisement);
-
-
-ALTER TABLE Image ADD CONSTRAINT FK_Image_0 FOREIGN KEY (ID_advertisement) REFERENCES Advertisement (ID_advertisement);
-
 
