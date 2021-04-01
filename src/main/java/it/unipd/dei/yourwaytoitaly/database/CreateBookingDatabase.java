e14cbed43b79efcf78e701a9a27d07bfaedd8127
Vittorio Esposito <vittorio.esposito.1@studenti.unipd.it>
2021-04-01 09:43:18 +0200

    01/04 09:43 writing CreateBookingDatabase.java

--- src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateBookingDatabase.java
+++ src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateBookingDatabase.java
@@ -1,8 +1,10 @@
 package it.unipd.dei.yourwaytoitaly.database;
 
 import it.unipd.dei.yourwaytoitaly.resource.Booking;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -14,17 +16,49 @@ import java.sql.SQLException;
  */
 
 public final class CreateBookingDatabase {
+
+    /**
+     * The SQL statement to be executed
+     */
     private static final String STATEMENT =
-            "INSERT INTO Booking (email_t, ID_Advertisement, date_b, time_b, num_booking, state) " +
+            "INSERT INTO YWTI.Booking (email_t, ID_Advertisement, date_b, time_b, num_booking, state) " +
                     "VALUES (?, ?, ?, ?, ?, ?);";
+    /**
+     * The connection to the database
+     */
     private final Connection con;
+
+    /**
+     * The booking to create
+     */
     private final Booking booking;
+
+    /**
+     * Creates a new object for creating bookings.
+     *
+     * @param con
+     *            the connection to the database.
+     * @param booking
+     *            the booking to create.
+     */
     public CreateBookingDatabase(final Connection con, final Booking booking) {
         this.con = con;
         this.booking = booking;
     }
-    public void createBooking() throws SQLException {
+
+    /**
+     * Creates a new booking.
+     *
+     * @return the just created booking
+     *
+     * @throws SQLException
+     *             if any error occurs while creating bookings.
+     */
+    public Booking createBooking() throws SQLException {
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        // the results of the creation
+        Booking b = null;
         try {
             pstmt = con.prepareStatement(STATEMENT);
             pstmt.setString(1, booking.getEmailTourist());
@@ -34,12 +68,25 @@ public final class CreateBookingDatabase {
             pstmt.setInt(5, booking.getNumBooking());
             pstmt.setString(6, booking.getState());
 
-            pstmt.execute();
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                b = new Booking(rs.getString("email_t"), rs.getInt("ID_advertisement"),
+                        rs.getDate("date_b"), rs.getTime("time_b"),
+                        rs.getInt("num_booking"), rs.getString("state"));
+            }
+
         } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
             if (pstmt != null) {
                 pstmt.close();
             }
+
             con.close();
         }
+        return b;
     }
 }
