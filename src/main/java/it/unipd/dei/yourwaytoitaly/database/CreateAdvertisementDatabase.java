05551597018e811b60d9347e8d1bae71b1ab218d
Vittorio Esposito <vittorio.esposito.1@studenti.unipd.it>
2021-04-01 09:49:41 +0200

    01/04 09:49 writing CreateAdvertisementDatabase.java

--- src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateAdvertisementDatabase.java
+++ src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateAdvertisementDatabase.java
@@ -1,8 +1,10 @@
 package it.unipd.dei.yourwaytoitaly.database;
 
 import it.unipd.dei.yourwaytoitaly.resource.Advertisement;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 
@@ -15,20 +17,53 @@ import java.sql.SQLException;
 
 
 public final class CreateAdvertisementDatabase {
+
+    /**
+     * The SQL statement to be executed
+     */
     private static final String STATEMENT =
             "INSERT INTO Advertisement (DESCRIPTION, SCORE, PRICE, NUM_TOT_ITEM, " +
                     "DATE_START, DATE_END, TIME_START, TIME_END, email_c, ID_TYPE) " +
                     "SELECT ?, ?, ?, ?, ?, ?, ?, ?, ?, pID_TYPE " +
                     "FROM Type_advertisement WHERE Type_advertisement.type = ?;";
+
+    /**
+     * The connection to the database
+     */
     private final Connection con;
+
+    /**
+     * The advertisement to create
+     */
     private final Advertisement advertisement;
+
+    /**
+     * Creates a new object for creating advertisements.
+     *
+     * @param con
+     *            the connection to the database.
+     * @param advertisement
+     *            the advertisement to create.
+     */
     public CreateAdvertisementDatabase(final Connection con, final Advertisement advertisement) {
         this.con = con;
         this.advertisement = advertisement;
     }
 
-    public void createAdvertisement() throws SQLException {
+    /**
+     * Creates a new advertisement.
+     *
+     * @return the just created advertisement
+     *
+     * @throws SQLException
+     *             if any error occurs while creating advertisements.
+     */
+    public Advertisement createAdvertisement() throws SQLException {
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        // the results of the creation
+        Advertisement a = null;
         try {
             pstmt = con.prepareStatement(STATEMENT);
             pstmt.setString(1, advertisement.getDescription());
@@ -42,12 +77,34 @@ public final class CreateAdvertisementDatabase {
             pstmt.setString(9, advertisement.getEmailCompany());
             pstmt.setInt(10, advertisement.getIdType());
 
-            pstmt.execute();
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                a = new Advertisement(
+                        rs.getInt("ID_advertisement"),
+                        rs.getString("description"),
+                        rs.getInt("score"),
+                        rs.getInt("price"),
+                        rs.getInt("num_tot_item"),
+                        rs.getDate("date_start"),
+                        rs.getDate("date_end"),
+                        rs.getTime("time_start"),
+                        rs.getTime("time_end"),
+                        rs.getString("email_c"),
+                        rs.getInt("ID_type"));
+            }
         } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
             if (pstmt != null) {
                 pstmt.close();
             }
+
             con.close();
         }
+
+        return a;
     }
 }
