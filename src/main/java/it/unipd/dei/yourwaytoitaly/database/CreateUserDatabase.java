b117e7f208a487f9e14922be18da876e880cd738
Marco <marco.basso.10@studenti.unipd.it>
2021-04-01 12:10:49 +0200

    1-4 12:10 User DOA fixed

--- src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateUserDatabase.java
+++ src/main/java/it/unipd/dei/yourwaytoitaly/database/CreateUserDatabase.java
@@ -4,6 +4,7 @@ import it.unipd.dei.yourwaytoitaly.resource.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -16,15 +17,17 @@ import java.sql.SQLException;
 
 public final class CreateUserDatabase {
 
-    private static final String STATEMENT_TOURIST = "INSERT INTO YWTIDB.Tourist " +
-            "(ID_user, surname, name, birth_date) VALUES (?, ?, ?, ?)";
+    private static final String STATEMENT_TOURIST = "INSERT INTO YWTIDB.Tourist (email_t, surname, name, birth_date, phone_number, address, password, ID_city)" +
+                                                    "SELECT ?, ?, ?, ?, ?, ?, ?, ID_CITY" +
+                                                    "FROM City WHERE City.name = ? RETURNING *;";
 
-    private static final String STATEMENT_COMPANY = "INSERT INTO YWTIDB.Company " +
-            "(ID_user, denomination) VALUES (?, ?)";
-
+    private static final String STATEMENT_COMPANY = "INSERT INTO YWTIDB.Company (email_c, name_c, phone_number, address, password, ID_city)" +
+                                                    "SELECT ?, ?, ?, ?, ?, ID_CITY" +
+                                                    "FROM City WHERE City.name = ? RETURNING *;";
 
     private final Connection con;
     private final User user;
+
     public CreateUserDatabase(final Connection con, final Tourist tourist) {
         this.con = con;
         this.user = tourist;
@@ -35,25 +38,64 @@ public final class CreateUserDatabase {
     }
     public User createUser() throws SQLException {
         PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
         User u = null;
+        
         try {
             if (this.user instanceof Tourist) {
-                /*pstmt = con.prepareStatement(STATEMENT_TOURIST);
-                pstmt.setInt(1, ((Tourist) user).getIdUser());
-                pstmt.setString(2, ((Tourist) user).getSurname());
-                pstmt.setString(3, ((Tourist) user).getName());
-                pstmt.setDate(4, ((Tourist) user).getBirthDate());
-                pstmt.execute();*/
+                pstmt = con.prepareStatement(STATEMENT_TOURIST);
+                pstmt.setString(1, ((Tourist) this.user).getEmail());
+                pstmt.setString(2, ((Tourist) this.user).getSurname());
+                pstmt.setString(3, ((Tourist) this.user).getName());
+                pstmt.setDate(4, ((Tourist) this.user).getBirthDate());
+                pstmt.setString(5, ((Tourist) this.user).getPhoneNumber());
+                pstmt.setString(6, ((Tourist) this.user).getAddress());
+                pstmt.setString(7, ((Tourist) this.user).getPassword());
+
+                rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+                    u = new Tourist(
+                            rs.getString("email_t"),
+                            rs.getString("password"),
+                            rs.getString("name"),
+                            rs.getString("address"),
+                            rs.getString("phone_number"),
+                            rs.getInt("ID_city"),
+                            rs.getString("surname"),
+                            rs.getDate("birth_date"));
+                }
             } else if (this.user instanceof Company) {
-                /*pstmt = con.prepareStatement(STATEMENT_COMPANY);
-                pstmt.setInt(1, ((Company) user).getIdUser());
-                pstmt.setString(2, ((Company) user).getDenomination());
-                pstmt.execute();*/
+                pstmt = con.prepareStatement(STATEMENT_COMPANY);
+                pstmt.setString(1, ((Company) this.user).getEmail());
+                pstmt.setString(2, ((Company) this.user).getName());
+                pstmt.setString(3, ((Company) this.user).getPhoneNumber());
+                pstmt.setString(4, ((Company) this.user).getAddress());
+                pstmt.setString(5, ((Company) this.user).getPassword());
+                pstmt.setInt(6, ((Company) this.user).getIdCity());
+                rs = pstmt.executeQuery();
+
+                while (rs.next()) {
+                    u = new Company(
+                            rs.getString("email_c"),
+                            rs.getString("password"),
+                            rs.getString("address"),
+                            rs.getString("phone_number"),
+                            rs.getInt("ID_city"),
+                            rs.getString("name_c"));
+                }
+
             }
         } finally {
+            if (rs != null) {
+                rs.close();
+            }
+
             if (pstmt != null) {
                 pstmt.close();
             }
+
             con.close();
         }
         return u;
