e64610e8c1978566669e1395c01835fb222d3f43
Francesco <francesco.giurisato@studenti.unipd.it>
2021-04-09 13:39:57 +0200

    created SessionCheck class to be used to check session and modified ShowUser

--- src/main/java/it/unipd/dei/yourwaytoitaly/servlet/ShowUserServlet.java
+++ src/main/java/it/unipd/dei/yourwaytoitaly/servlet/ShowUserServlet.java
@@ -1,19 +1,19 @@
 package it.unipd.dei.yourwaytoitaly.servlet;
 
 import it.unipd.dei.yourwaytoitaly.database.UserDAO;
-import it.unipd.dei.yourwaytoitaly.resource.Company;
-import it.unipd.dei.yourwaytoitaly.resource.Message;
-import it.unipd.dei.yourwaytoitaly.resource.Tourist;
-import it.unipd.dei.yourwaytoitaly.resource.User;
+import it.unipd.dei.yourwaytoitaly.resource.*;
 import it.unipd.dei.yourwaytoitaly.utils.ErrorCode;
 import org.json.JSONObject;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.StringReader;
+import java.sql.SQLException;
+import java.util.List;
 
 
 /**
@@ -26,51 +26,53 @@ import java.io.StringReader;
 public class ShowUserServlet extends AbstractDatabaseServlet {
 
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        String op = req.getRequestURI();
+        op = op.substring(op.lastIndexOf("user") + 5);
 
-        try {
+        switch (op){
+            case "profile/":
+                try {
 
-            // check if a session is valid
-            HttpSession session = req.getSession(false);
-            if (session == null || session.getAttribute("email")==null) {
-                session.invalidate();
-                req.getRequestDispatcher("/jsp/login.jsp").forward(req, res);
+                    User u = new SessionCheckServlet(req, res).getUser();
+
+                    JSONObject resJSON = new JSONObject();
+
+                    if (u instanceof Tourist) {
+                        resJSON.put("email", u.getEmail());
+                        resJSON.put("name", u.getName());
+                        resJSON.put("surname", ((Tourist) u).getSurname());
+                        resJSON.put("birth-date", ((Tourist) u).getBirthDate());
+                        resJSON.put("email", u.getEmail());
+                        resJSON.put("address", u.getAddress());
+                        resJSON.put("phone-number", u.getPhoneNumber());
+                    } else if (u instanceof Company) {
+                        resJSON.put("email", u.getEmail());
+                        resJSON.put("name", u.getName());
+                        resJSON.put("email", u.getEmail());
+                        resJSON.put("address", u.getAddress());
+                        resJSON.put("phone-number", u.getPhoneNumber());
+                    }
+
+                    res.setStatus(HttpServletResponse.SC_OK);
+                    res.setContentType("application/json");
+                    res.getWriter().write((new JSONObject()).put("data", resJSON).toString());
+
+                } catch (SQLException | NamingException ex) {
+                    ErrorCode ec = ErrorCode.USER_NOT_FOUND;
+                    Message m = new Message("Cannot show the user.",
+                            ec.getErrorCode(), ex.getMessage());
+                    res.setStatus(ec.getHTTPCode());
+                    req.setAttribute("message", m);
+                    req.getRequestDispatcher("/jsp/show-message.jsp").forward(req, res);
+                }
+                break;
+            default:
+                ErrorCode ec = ErrorCode.OPERATION_UNKNOWN;
+                Message m = new Message("Operation unknown. ",
+                        ec.getErrorCode(), ec.getErrorMessage());
+                res.setStatus(ec.getHTTPCode());
+                req.setAttribute("message", m);
+                req.getRequestDispatcher("/jsp/show-message.jsp").forward(req, res);
             }
-
-            // get email and password of the user from the session
-            String emailSession = session.getAttribute("email").toString();
-            String passwordSession = session.getAttribute("password").toString();
-
-            User u = UserDAO.searchUserLogin(emailSession, passwordSession);
-
-            JSONObject resJSON = new JSONObject();
-
-            if (u instanceof Tourist) {
-                resJSON.put("email", u.getEmail());
-                resJSON.put("name", u.getName());
-                resJSON.put("surname", ((Tourist) u).getSurname());
-                resJSON.put("birth-date", ((Tourist) u).getBirthDate());
-                resJSON.put("email", u.getEmail());
-                resJSON.put("address", u.getAddress());
-                resJSON.put("phone-number", u.getPhoneNumber());
-            } else if (u instanceof Company) {
-                resJSON.put("email", u.getEmail());
-                resJSON.put("name", u.getName());
-                resJSON.put("email", u.getEmail());
-                resJSON.put("address", u.getAddress());
-                resJSON.put("phone-number", u.getPhoneNumber());
-            }
-
-            res.setStatus(HttpServletResponse.SC_OK);
-            res.setContentType("application/json");
-            res.getWriter().write((new JSONObject()).put("data", resJSON).toString());
-
-        } catch (Exception ex) {
-            ErrorCode ec = ErrorCode.INTERNAL_ERROR;
-            Message m = new Message("Cannot show the user. ",
-                    ec.getErrorCode(), ex.getMessage());
-            res.setStatus(ec.getHTTPCode());
-            req.setAttribute("message", m);
-            req.getRequestDispatcher("/jsp/show-message.jsp").forward(req, res);
-        }
     }
 }
