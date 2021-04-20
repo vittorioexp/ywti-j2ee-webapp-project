c5cee42ed14d050572d75b228542cbf3c60d454b
Francesco <francesco.giurisato@studenti.unipd.it>
2021-04-20 09:51:08 +0200

    HttpRequestCheck

---
+++ src/main/java/it/unipd/dei/yourwaytoitaly/filter/HttpRequestCheck.java
@@ -0,0 +1,66 @@
+package it.unipd.dei.yourwaytoitaly.filter;
+
+import it.unipd.dei.yourwaytoitaly.servlet.LoginServlet;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.util.Base64;
+
+/**
+ * Servlet class, to be written
+ * @author Vittorio Esposito
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class HttpRequestCheck implements Filter {
+
+    /*
+     * The configuration for the filter
+     * */
+    private FilterConfig config = null;
+    /*
+     * The connection to the database
+     * */
+    private DataSource ds;
+
+    @Override
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        if (filterConfig == null) {
+            throw new ServletException("Filter configuration cannot be null");
+        }
+        this.config = filterConfig;
+    }
+
+    /** Method filtering requests that are not HTTP
+     * @param servletRequest ServletRequest
+     * @param servletResponse ServletRersponse
+     * @param filterChain FilterChain
+     * */
+    @Override
+    public void doFilter(final ServletRequest servletRequest, final ServletResponse servletResponse, final FilterChain filterChain) throws IOException, ServletException {
+
+        if (!(servletRequest instanceof HttpServletRequest) || !(servletResponse instanceof HttpServletResponse)){
+            throw new ServletException("Only HTTP requests or response are accepted");
+        }
+
+        final HttpServletRequest req = (HttpServletRequest) servletRequest;
+        final HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        filterChain.doFilter( req , res );
+
+    }
+
+    @Override
+    public void destroy() {
+        config = null;
+        ds = null;
+    }
+
+}
+
