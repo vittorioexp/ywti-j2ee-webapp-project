1a70a2b7f254f4ced7d8f5e12930be3c838b95d0
Vittorio Esposito <vittorio.esposito.1@studenti.unipd.it>
2021-05-07 09:00:52 +0200

    07/05 implementing js in the homepage

--- src/main/webapp/js/index-page.js
+++ src/main/webapp/js/index-page.js
@@ -2,52 +2,52 @@
 // TODO Vedere tutor repo: js/search-maintenance-page.js
 
 // self executing function here
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener("DOMContentLoaded", function(event) {
     // your page initialization code here
     // the DOM will be available here
 
-    // Requests and shows the list of typeAdvertisements and Cities
-    getTypeAdvertisementList();
-    getCityList();
+    // Fetches the list of typeAdvertisements and Cities
+    fetchTypeAdvertisementList();
+    fetchCityList();
 
     // calls searchAdvertisements() when clicking the search button
-    document.getElementById("search-button").addEventListener("click", getAdvertisementList);
+    document.getElementById("search-button").addEventListener("click", fetchAdvertisementList);
 });
 
 // TODO: Request the list of available typeAdvertisement
-function getTypeAdvertisementList() {
+function fetchTypeAdvertisementList() {
     var url = new URL(contextPath+'/.....');
     // put code here
     genericGETRequest(url, showTypeAdvertisementList);
 }
 
 // TODO: Shows the list of available typeAdvertisement just requested
-function showTypeAdvertisementList(req){
+function loadTypeAdvertisementList(req){
 }
 
 // TODO: Request the list of available cities
-function getCityList(){
+function fetchCityList(){
     var url = new URL(contextPath+'/.....');
     // put code here
     genericGETRequest(url, showCityList);
 }
 
 // TODO: Shows the list of available cities just requested
-function showCityList(req){
+function loadCityList(req){
     var url = new URL(contextPath+'/adv');
     // put code here
     genericGETRequest(url, showCityList);
 }
 
 // TODO: Converts the form in JSON format and requests the list of advertisements
-function getAdvertisementList(){
+function fetchAdvertisementList(){
     var url = new URL(contextPath+'/adv');
     //url.searchParams.set("idType", document.getElementById("idType").value);
     //url.searchParams.set("idCity", document.getElementById("idCity").value);
     //url.searchParams.set("dateStart", document.getElementById("dateStart").value);
-    genericGETRequest(url, showAdvertisementList);
+    genericGETRequest(url, loadAdvertisementList);
 }
 
 // TODO: Shows the list of advertisements
-function showAdvertisementList(req){
+function loadAdvertisementList(req){
 }
