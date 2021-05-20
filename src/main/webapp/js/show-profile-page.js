31691e9a8964bc8abc94d14d7447144713bf94b8
Marco <marco.basso.10@studenti.unipd.it>
2021-05-20 18:11:39 +0200

    show profile info login

--- src/main/webapp/js/show-profile-page.js
+++ src/main/webapp/js/show-profile-page.js
@@ -80,6 +80,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
     } else if (getUserRole()==="tourist") {
 
         let deleteBookingButtons = document.getElementsByName("deleteBookingButton");
+        let showBookingButton = document.getElementsByName("showBookingButton");
+
+        showBookingButton.forEach(function(button, index) {
+
+            // Adds an event listener (on click) on each delete booking button
+            button.addEventListener(
+                "click",
+                function(event) {
+                    event.preventDefault();
+                    // Redirect to the show advertisement html
+                    let idAdvertisement = button.value;
+                    window.location.href = contextPath + "/adv-show/" + idAdvertisement;
+                });
+        });
 
         deleteBookingButtons.forEach(function(button, index) {
 
@@ -91,7 +105,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     if(confirm("Are you sure you want to cancel the reservation?")){
                         // Send an http delete to the server
                         let idAdvertisement = button.value;
-                        console.log(idAdvertisement);
                         let url = contextPath + "/booking-delete";
                         let data = {
                             "idAdvertisement": idAdvertisement
